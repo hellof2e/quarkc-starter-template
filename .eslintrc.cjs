eda17f124f503a20dbf8cc0a4a54443b5f2489e2
xsf0105 <allan91coder@gmail.com>
2023-10-26 14:39:37 +0800

    feat: first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+// https://typescript-eslint.io/getting-started
+
+
+/* eslint-env node */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+<div align="center">
+
+<h1>用 Quarkc 构建高性能前端页面</h1>
+<h4>Build web application with Quarkc x Gluang</h4>
+
+# Develop & Installation
+
+开发，构建和运行。
+
+```bash
+npm install
+npm run dev
+```
